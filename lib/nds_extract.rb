1fcbfa9b7ca9ad9e5ef4961fecc7e90281ac81c6
candescent-ninja-2792 <mflynn333@gmail.com>
2019-11-19 02:11:12 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -4,6 +4,8 @@ require 'directors_database'
 # A method we're giving you. This "flattens"  Arrays of Arrays so: [[1,2],
 # [3,4,5], [6]] => [1,2,3,4,5,6].
 
+pp directors_database
+
 
 def flatten_a_o_a(aoa)
   result = []
